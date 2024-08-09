01bcb4e53e9826e0d3cb4db67299a0dd2ea3967b
Simon Mourier <smourier@users.noreply.github.com>
2024-08-09 08:52:35 +0200

    Initial version

---
+++ ExplorerDataProvider/framework.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <windows.h>
+#include <initguid.h>
+#include <shlobj.h>
+#include <ShlGuid.h>
+#include <propkey.h>
+#include <shlwapi.h>
+#include <strsafe.h>
+#include <shellapi.h>
+#include <olectl.h>
+#include <new>  // std::nothrow
+
